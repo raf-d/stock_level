38c260eee70a91804deeab94393d0a679423f47a
dyzio812 <dyzio812@gmail.com>
2021-02-11 10:42:24 +0100

    fixed some issues

--- stocklevel/stocklevel/settings.py
+++ stocklevel/stocklevel/settings.py
@@ -108,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 TIME_ZONE = 'UTC'
 
